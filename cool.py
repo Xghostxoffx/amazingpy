ed18ef35c2721ebd28fa393e28bd8f69e3dc4f2d
Xghostxoffx <xgh0ustxdz@gmail.com>
2024-07-28 01:16:20 +0100

    Add files via upload

---
+++ cool.py
@@ -0,0 +1,51 @@
+def brown():
+    BROWN = "\033[0;33m"
+    return (BROWN)
+def light_gray():
+    LIGHT_GRAY = "\033[0;37m"
+    return(LICHT_GRAY)
+def dark_gray():
+    DARK_GRAY = "\033[1;30m"
+    return(DARK_GRAY)
+def light_red(): 
+    LIGHT_RED = "\033[1;31m"
+    return(LIGHT_RED)
+def light_green():
+    LIGHT_GREEN = "\033[1;32m"
+    return(LIGHT_GREEN)
+def light_blue():
+    LIGHT_BLUE = "\033[1;34m"
+    return(LIGHT_BLUE)
+def light_purple():
+    LIGHT_PURPLE = "\033[1;35m"
+    return (LIGHT_PURPLE)
+def light_cyan():
+    LIGHT_CYAN = "\033[1;36m"
+    return(LIGHT_CYAN)
+def light_white():
+    LIGHT_WHITE = "\033[1;37m"
+    return(LIGHT_WHITE)
+def red():
+    red = "\033[1;31m" # Red
+    return (red)
+def black():
+    black="\033[1;30m" # Black
+    return (black) 
+def  green():
+    green ="\033[1;32m" # Green
+    return (green)
+def yellow():
+    yellow ="\033[1;33m" # Yellow
+    return (yellow)
+def blue():
+    blue = "\033[1;34m" # Blue
+    return (blue)
+def purple():
+    purple = "\033[1;35m" # Purple
+    return (purple)
+def cyan():
+    cyan="\033[1;36m" # Cyan
+    return (cyan)
+def white():
+    white="\033[1;37m" # White
+    return (white)
